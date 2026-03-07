a435252deea6d78b3a217e2e31624f79aba8ba05
SalmanIbrahimShaikh <salmanshaikh1693@gmail.com>
2026-03-07 22:50:59 +0530

    change db to sqlite

--- overdue_service/settings.py
+++ overdue_service/settings.py
@@ -78,14 +78,21 @@ WSGI_APPLICATION = 'overdue_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'task_manager',   # Laravel DB name
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'task_manager',   # Laravel DB name
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
